0271a07fcae37268366abe9b7b20e3dea1ecbb9c
Aleksandar Stojanovic <petaq@Aleksandars-MacBook-Air.local>
2022-12-19 12:51:46 +0100

    feat: implement global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
 import { AppContainer } from "./styles";
+import { useAppState } from "./state/AppStateContext";
 
 export function App() {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add new list"
         onAdd={() => console.log("Item created")}
