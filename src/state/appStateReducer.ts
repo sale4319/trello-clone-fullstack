ad66e8da685b120e04d817729da51837da97a62d
Aleksandar Stojanovic <aleksandarstojanovic@Aleksandars-MacBook-Pro.local>
2023-02-01 17:48:32 +0100

    feat: added move card functionallity

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -49,6 +49,29 @@ export const appStateReducer = (
       draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
       break;
     }
+    case "MOVE_TASK": {
+      const { draggedItemId, hoveredItemId, sourceColumnId, targetColumnId } =
+        action.payload;
+      const sourceListIndex = findItemIndexById(draft.lists, sourceColumnId);
+      const targetListIndex = findItemIndexById(draft.lists, targetColumnId);
+
+      const dragIndex = findItemIndexById(
+        draft.lists[sourceListIndex].tasks,
+        draggedItemId
+      );
+      const hoverIndex = hoveredItemId
+        ? findItemIndexById(draft.lists[targetListIndex].tasks, hoveredItemId)
+        : 0;
+
+      const item = draft.lists[sourceListIndex].tasks[dragIndex];
+
+      draft.lists[sourceListIndex].tasks.splice(dragIndex, 1);
+
+      draft.lists[targetListIndex].tasks.splice(hoverIndex, 0, item);
+
+      break;
+    }
+
     case "SET_DRAGGED_ITEM": {
       draft.draggedItem = action.payload;
       break;
